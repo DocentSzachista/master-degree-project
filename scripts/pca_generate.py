99e80364f0310eac0c1acf3a22e454bad7150ed0
Damian Raczkowski <idontknow5325@gmail.com>
2023-07-12 18:02:16 +0200

    Add data mark option for PCA diagram
    Signed-off-by: Damian Raczkowski <idontknow5325@gmail.com>

--- scripts/pca_generate.py
+++ scripts/pca_generate.py
@@ -1,10 +1,12 @@
 import argparse
-from os import makedirs, path 
+from os import makedirs, path
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 import re
+from sample_picker import pick_chosen_label
+import matplotlib.colors as mcolors
 
 LABELS_CIFAR_10 = {
     0: "airplane",
@@ -30,15 +32,20 @@ def prepare_script() -> (argparse.Namespace, str):
     parser.add_argument(
         "--test_file", required=False, help="location of test pickle dataset."
     )
-    parser.add_argument("-o", "--output", required=True, help="Dir to save generated file")
+    parser.add_argument(
+        "-o", "--output", required=True, help="Dir to save generated file"
+    )
     args = parser.parse_args()
     output_title = re.findall(r"\w+\.", args.train_file)
 
     return args, output_title[0]
 
 
-def make_visualization(df: pd.DataFrame, labels: dict, plot_title: str,
-                       save_dir: str | None = None) -> None:
+def generate_pca_scatter_plot(
+    df: pd.DataFrame,
+    labels: dict,
+    plot_title: str,
+) -> None:
     """Visualize PCA
 
     Draw model's class distribution on a scatter plot.
@@ -49,28 +56,59 @@ def make_visualization(df: pd.DataFrame, labels: dict, plot_title: str,
         Dataframe containing PCA and original labels keys
     labels: dict
         Labels for each of possible classifier values.
-    save_dir: str, None = False
-        Save directory for generated image, if none was provided, just display it
+
+    Returns
+    -------
+    Figure and Axes objects.
     """
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(1, 1, 1)
     ax.set_xlabel("PCA 1", fontsize=15)
     ax.set_ylabel("PCA 2", fontsize=15)
     ax.set_title(plot_title, fontsize=20)
-
-    for label in labels.keys():
+    colors = mcolors.TABLEAU_COLORS.keys()
+    for color, label in zip(colors, labels.keys()):
         indices_to_keep = df["original_label"] == label
-        ax.scatter(df.loc[indices_to_keep, "PC1"], df.loc[indices_to_keep, "PC2"], s=50)
-    ax.legend(labels.values())
+        ax.scatter(
+            df.loc[indices_to_keep, "PC1"],
+            df.loc[indices_to_keep, "PC2"],
+            label=labels[label],
+            s=50,
+            c=color
+        )
+    ax.legend()
     ax.grid()
-    if save_dir:
-        plt.savefig(save_dir)
-    else:
-        plt.plot()
+    return fig, ax
 
 
-def prepare_pca(dataframe_train: pd.DataFrame,
-                dataframe_test: pd.DataFrame | None = None) -> pd.DataFrame:
+def mark_chosen_datapoints(
+    axis: plt.Axes, pca: PCA, points_dataframe: pd.DataFrame
+) -> plt.Axes:
+    """Mark datapoints on already existing Axes object.
+
+    Parameters:
+    -----------
+    axis: Axes,
+        Axis where additional points will be mapped.
+    pca: PCA
+        Pretrained PCA object which will reduce dimensions of provided points.
+    points_dataframe: Dataframe
+        Dataframe containing points to be marked on scatter plot.
+
+    Returns:
+    --------
+    Modified Axes object.
+    """
+    features = np.vstack(points_dataframe.features.values)
+    reduced_X = pca.transform(features)
+    df = pd.DataFrame(reduced_X, columns=["PC1", "PC2"])
+    axis.scatter(df.loc[:, "PC1"], df.loc[:, "PC2"], s=100, marker="x", c="k")
+    return axis
+
+
+def prepare_pca(
+    dataframe_train: pd.DataFrame, dataframe_test: pd.DataFrame | None = None
+) -> pd.DataFrame:
     """Prepare dataset for PCA visualization.
 
     Make dimension reduction to 2D by using PCA algorithm.
@@ -98,43 +136,63 @@ def prepare_pca(dataframe_train: pd.DataFrame,
     X_reduced = pca.transform(test_features)
     df = pd.DataFrame(X_reduced, columns=["PC1", "PC2"])
     final_df = pd.concat([df, labels], axis=1)
-    return final_df
+    return final_df, pca
 
 
-def run(train_input_dataframe_path: str, plot_title: str,
-        test_input_dataframe_path: str | None = None,
-        labels: dict | None = LABELS_CIFAR_10,
-        output_path: str | None = None) -> None:
-    """Util function to run PCA generation
+def run(
+    train_input_dataframe_path: str,
+    plot_title: str,
+    test_input_dataframe_path: str | None = None,
+    picked_datapoints: pd.DataFrame | None = None,
+    labels: dict | None = LABELS_CIFAR_10,
+    output_path: str | None = None,
+) -> None:
+    """Util function to run PCA generation.
 
     Parameters
     ----------
     input_dataframe_path: str
-        Path to pickle file
+        Path to train pickle file.
     plot_title: str
         Name of generated plot
+    test_input_dataframe_path: str, optional
+        Path to test pickle file.
+    picked_datapoints: DataFrame, optional
+        Dataframe containing picked points to show their exact location on PCA diagram.
     labels: dict, optional
-        Labels describing keys in the dataframe. If not provided it will use CIFAR_10 labels
+        Labels describing keys in the dataframe. If not provided it will use CIFAR_10 labels.
     output_file: str, optional
         Directory to the file where it should save plot.
-    """    
+    """
     train_dataframe = pd.read_pickle(train_input_dataframe_path)
     if test_input_dataframe_path is None:
         PCA_df = prepare_pca(train_dataframe)
     else:
         test_df = pd.read_pickle(test_input_dataframe_path)
-        PCA_DF = prepare_pca(train_dataframe, test_df)
-    make_visualization(PCA_df, labels, plot_title, output_path)
+        PCA_df = prepare_pca(train_dataframe, test_df)
+    fig, axis = generate_pca_scatter_plot(PCA_df, labels, plot_title)
+    if picked_datapoints is not None:
+        mark_chosen_datapoints(axis, PCA_df, picked_datapoints)
+    if output_path is None:
+        plt.show()
+    else: 
+        plt.savefig(output_path)
 
 
 if __name__ == "__main__":
     args, output_title = prepare_script()
     df_train = pd.read_pickle(args.train_file)
-    if args.test_file is not None: 
+    if args.test_file is not None:
         df_test = pd.read_pickle(args.test_file)
-    else: 
+    else:
         df_test = df_train
 
-    PCA_df = prepare_pca(df_train, df_test)
-    make_visualization(PCA_df, LABELS_CIFAR_10, output_title, 
-                       f"{args.output}/{output_title}.png")
+    PCA_df, pca = prepare_pca(df_train, df_test)
+    # make_visualization(PCA_df, LABELS_CIFAR_10, output_title,
+    #                    f"{args.output}/{output_title}.png")
+    (_, axis) = generate_pca_scatter_plot(PCA_df, LABELS_CIFAR_10, output_title)
+
+    points = pick_chosen_label(df_test, 4)
+    mark_chosen_datapoints(axis, pca, points.head(5))
+    plt.savefig(f"{args.output}/{output_title}_test_pca.png")
+
---
+++ scripts/sample_picker.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np 
+
+def pick_chosen_label(dataframe: pd.DataFrame, label: int) -> pd.DataFrame:
+    """ Pick chosen label sorted descending.   
+
+        Function filters dataframe to retrieve only desired label and
+        later sorts it by its confidence in classifier column
+
+        Parameters
+        ----------
+        label: int
+            Class label to retrieve from dataframe.
+
+        Returns
+        -------
+        Dataframe with selected label and sorted from highest confidence
+        to the lowest.
+
+    """
+    dataframe = dataframe[dataframe["original_label"] == label]  
+    sorted_indices = np.argsort(dataframe['classifier'].apply(lambda x: x[label]))[::-1]
+    return dataframe.iloc[sorted_indices]
